33540de1666e4058e98b58553a922fe2754937b9
satyajeet kumar jha <coderboysatya@gmail.com>
2017-02-27 09:40:00 +0530

    Update views.py

--- myproject/personal/views.py
+++ myproject/personal/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'personal/home.html')
 def contact(request):
-    return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me.','hskinsley@gmail.com']})
+    return render(request, 'personal/basic.html',{'content':['my phone no 12344567899999999 ']})
